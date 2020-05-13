ba5ce93a8df5fbdbf98d5a30c8bc7cc62d94eb25
Uriah Harston <uharston@gmail.com>
2020-05-13 14:25:41 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -14,19 +14,19 @@ class String
     self.end_with?("!")
   end
 
-  def count_sentences 
+  def count_sentences
     #find how to split the sentences into seperate element in an array with three arguments ( . ? !)
-    self.split(/[\.?!]/).length 
+    self.split(/[\.?!]/).length
   end
 end
 
 # stringy = "I am trying to learn split. It is not easy. What do you think? I will get it though!"
-# 
+#
 # #delete empty spaces in array
 # value = "cat,,dog,bird"
 # elements = value.split(",")
 # print elements, "\n"
-# 
+#
 # # Remove empty elements from the array.
 # elements.delete_if{|e| e.length == 0}
 # print elements
